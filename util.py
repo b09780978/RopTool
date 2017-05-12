68f1b16bb7787ab0bac94dd924b5a81ed789b14c
faker <b09780978@gmail.com>
2017-05-12 18:56:33 +0800

    add comment

--- util.py
+++ util.py
@@ -1,9 +1,11 @@
 #!/usr/bin/env python
 import struct
 
+# parse 32bit integer to byte
 def p32(data):
     return struct.pack("<I", data)
 
+# parse string to bytes(4 bytes)
 def pStr(word):
     if len(word)>4:
         return None
