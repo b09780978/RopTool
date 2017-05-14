84058aa0e9e1248e4aab3bba5e15a5fab9212ed8
faker <b09780978@gmail.com>
2017-05-15 02:34:34 +0800

    fix to rm pyc file after test

--- test.sh
+++ test.sh
@@ -1,7 +1,7 @@
 #!/usr/bin/env sh
-# clean all pyc file, it will influence test
-rm *.pyc
 # generate a script call auto.py and execute it
 python main.py src32 > auto.py
 python auto.py
 rm auto.py
+# clean all pyc file, it will influence test
+rm *.pyc
